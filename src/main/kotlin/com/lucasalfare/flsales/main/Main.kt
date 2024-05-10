2df2ca3e9652517b06e39fe098fa58107b19774d
LucasAlfare <god-lucas@hotmail.com>
2024-05-10 12:00:35 -0300

    turn port to other value

--- src/main/kotlin/com/lucasalfare/flsales/main/Main.kt
+++ src/main/kotlin/com/lucasalfare/flsales/main/Main.kt
@@ -104,7 +104,7 @@ suspend fun main() {
     }
   }
 
-  embeddedServer(Netty, port = 80) {
+  embeddedServer(Netty, port = 22) {
     configureCORS()
     configureSerialization()
     configureRouting()
